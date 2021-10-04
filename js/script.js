d30897dc4d6d8d54dd27b64b8429f0f9c6b22031
Gattem Bharath <53399475+GattemBharath@users.noreply.github.com>
2021-10-04 21:01:53 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const navLinks = document.querySelector(".nav-links");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const toggleImg = document.querySelector("#toggle-img");
+hamburgerBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  const isOpen = navLinks.classList.contains("open");
+  if (isOpen) {
+    toggleImg.src = "./images/icon-close.svg";
+  } else {
+    toggleImg.src = "./images/icon-hamburger.svg";
+  }
+});
